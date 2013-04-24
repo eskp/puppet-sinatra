5d5d860f82d77ea2be4d9748f9accf62722841eb
eskp <si@eskp.net>
2013-04-24 10:03:28 +1000

    remove utils from node def

--- nodes/test.node.pp
+++ nodes/test.node.pp
@@ -1,5 +1,5 @@
 node 'ip-10-240-10-140.ap-southeast-2.compute.internal' {
-  include iptables, nginx, ssh, utils
+  include iptables, nginx, ssh
 
   app::deploy { '/var/www':
     address => 'https://github.com/tnh/simple-sinatra-app.git',
